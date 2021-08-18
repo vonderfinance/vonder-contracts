17df1a7da34c42fb8104a07e41f6e0f1d19902ae
korrio <iamnotkorr@gmail.com>
2021-08-18 08:37:23 +0700

    test mint & redeem on BSC testnet -> pass need to fix redeem business logic (fee)

--- contracts/VDPMaster.sol
+++ contracts/VDPMaster.sol
@@ -1,7 +1,3 @@
-/**
- *Submitted for verification at BscScan.com on 2021-08-03
-*/
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.12;
--- migrations/5_deploy_vdpmaster.js
+++ migrations/5_deploy_vdpmaster.js
@@ -1,21 +1,25 @@
 const VDPMaster = artifacts.require("VDPMaster");
-const XVON = artifacts.require("XVonderToken");
+// const XVON = artifacts.require("XVonderToken");
 const VDP = artifacts.require("VDP");
 const Web3 = require("web3");
 
 module.exports = (deployer, network, addresses) => {
-		const [admin] = addresses;
+    const [admin] = addresses;
 
     const BUSD = {
-    	address: "0x8fD45Fe67984a325E823C091238596984041b9eB"
+        address: "0x322e4612792dcdd60a50f9802c823e7b51ef1e31"
+    };
+
+    const XVON = {
+        address: "0x8fd45fe67984a325e823c091238596984041b9eb"
     };
 
     const TREASURY = {
-    	address: admin
+        address: "0x1f13c3e96D3243a97F90Fba32CA2E3525A38976b"
     }
 
     const maxStakeAmount = Web3.utils.toWei('1000', 'ether');
 
     // IVDP _vdp, IERC20 _busd, IERC20 _xvon, address _treasury, uint256 _maxStakeAmount
-    deployer.deploy(VDPMaster, VDP.address, BUSD.address,XVON.address,TREASURY.address,maxStakeAmount, { from: admin, gas: '3000000' });
+    deployer.deploy(VDPMaster, VDP.address, BUSD.address, XVON.address, TREASURY.address, maxStakeAmount, { from: admin, gas: '3000000' });
 };
